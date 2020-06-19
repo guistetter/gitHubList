0bbeba1d598d5c68a510ecc50486b7ef149d494e
guistetter <guilherme_stetter@hotmail.com>
2020-06-19 11:10:21 -0300

    configurando reactotron

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import './config/ReactotronConfig';
+console.tron.log('ola');
 export default class App extends Component {
   render() {
     return (
-      <View style={StyleSheet.container}>
+      <View style={styles.container}>
         <Text>Ola my app</Text>
       </View>
     );
@@ -12,5 +14,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
